e0bfbb9ca36a792068abe7bf51886a4b8602dae9
Mohammed Althaf H <althaf78602@gmail.com>
2026-08-17 02:33:16 +0530

    feat: add OS-wide global hotkey (Ctrl+Shift+Space) to summon Intodo anywhere on Windows

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,32 @@
+use tauri::{Manager};
+use tauri_plugin_global_shortcut::{GlobalShortcutExt, Shortcut, ShortcutState};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::default().build())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .setup(|app| {
+            // Register Global Shortcut: Ctrl + Shift + Space
+            let shortcut: Shortcut = "Ctrl+Shift+Space".parse().unwrap();
+            let app_handle = app.handle().clone();
+            
+            let _ = app.global_shortcut().on_shortcut(shortcut, move |_app, _shortcut, event| {
+                if event.state() == ShortcutState::Pressed {
+                    if let Some(window) = app_handle.get_webview_window("main") {
+                        if window.is_visible().unwrap_or(false) && window.is_focused().unwrap_or(false) {
+                            let _ = window.minimize();
+                        } else {
+                            let _ = window.show();
+                            let _ = window.unminimize();
+                            let _ = window.set_focus();
+                        }
+                    }
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
